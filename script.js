27913a3ca0c73765a812d4574bee9a0a8c5ca8d2
kmgliya <85997386+kmgliya@users.noreply.github.com>
2021-08-11 23:19:49 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+
+
+// let obj = {year: 2025, month: 12,day: 31,};
+// const obj = {
+//     year: 2025,
+//     month: 12,
+//     day: 31
+// };
+// const {year: newYear,month: newMonth, day: newDay}= obj ;  //оригинально)☻
+// console.log(newYear,newMonth,newDay);
+
+
+// let arr = [2025, 12, 31];
+// let [year, month, day]= arr;
+// console.log(year, month, day);
+
+
+// const myHonda = {
+//     color: 'red',
+//     wheels: 4,
+//     engine: {
+//         cylinders: 4,
+//         size: 2.2
+//     }
+// }
+//
+// delete myHonda.wheels
+//
+// myHonda.year = 2010
+//
+// myHonda.color = 'yellow'
+//
+// myHonda.func = function(name){
+//     console.log(`Hello ${name}`)
+// }
+//
+// for(let key in myHonda){
+//     if(typeof myHonda[key] !== 'object'){
+//         console.log(`${key} - ${myHonda[key]}`)
+//     }
+//     else{
+//         for(let newKey in myHonda[key]){
+//             console.log(`${newKey} - ${myHonda[key][newKey]}`)
+//         }
+//     }
+// }
+//
+// myHonda.func('Name') ☻
